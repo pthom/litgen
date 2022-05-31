b6b1bb57e17800ab090bfc275e00014982dd78a9
Pascal Thomet <pthomet@gmail.com>
2022-06-01 00:14:22 +0200

    Remove old implementations

--- litgenold/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-import os, sys; _THIS_DIR = os.path.dirname(__file__); sys.path = [_THIS_DIR] + sys.path
-from code_types import *
--- litgenold/tests/function_generator_test.py
+++
@@ -1,7 +0,0 @@
-import os, sys; _THIS_DIR = os.path.dirname(__file__); sys.path = [_THIS_DIR + "/.."] + sys.path
-import code_utils
-from code_types import *
-from options import CodeStyleOptions, code_style_immvision, code_style_implot
-import function_generator, function_parser
-
-
