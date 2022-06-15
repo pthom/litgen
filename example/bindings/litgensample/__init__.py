290454b4951c22038f9b0cb13803b99f04abad6c
Pascal Thomet <pthomet@gmail.com>
2022-06-15 23:50:48 +0200

    Disable black formatter on generated code

--- example/bindings/litgensample/__init__.py
+++ example/bindings/litgensample/__init__.py
@@ -1,2 +1,5 @@
-import os, sys; this_dir = os.path.dirname(__file__); sys.path = [this_dir] + sys.path
+import os, sys
+
+this_dir = os.path.dirname(__file__)
+sys.path = [this_dir] + sys.path
 from _cpp_litgensample import *
