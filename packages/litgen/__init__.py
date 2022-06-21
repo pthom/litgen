aad6e87cf358cf40dba769f75177bd7fbe7ffaac
Pascal Thomet <pthomet@gmail.com>
2022-06-21 15:44:23 +0200

    fix CI

--- packages/litgen/__init__.py
+++ packages/litgen/__init__.py
@@ -1,3 +1,3 @@
 from codemanip.code_replacements import standard_replacements
 from litgen.generate_code import generate_files, generate_pydef, generate_pyi
-from litgen.options import LitgenOptions, code_style_implot
+from litgen.options import LitgenOptions, code_style_implot, code_style_imgui
