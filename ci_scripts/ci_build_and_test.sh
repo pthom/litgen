60fe6c8001e28f5ef6c138e6b8b4a6c451e05360
Pascal Thomet <pthomet@gmail.com>
2022-07-05 16:37:11 +0200

    ci_build_and_test.sh: Check that lg_imgui and lg_hello_imgui use the same imgui version

--- ci_scripts/ci_build_and_test.sh
+++ ci_scripts/ci_build_and_test.sh
@@ -19,35 +19,51 @@ $decoration
 }
 
 
+title "Check that lg_imgui and lg_hello_imgui use the same imgui version"
+lg_projects_dir=$REPO_DIR/examples_real_libs
+lg_project=imgui
+cd "$lg_projects_dir"/$lg_project/external/imgui && git --no-pager log --format="%H" -n 1 > "$lg_projects_dir"/"$lg_project".githead && cd "$lg_projects_dir"
+lg_project=hello_imgui
+cd "$lg_projects_dir"/$lg_project/external/imgui && git --no-pager log --format="%H" -n 1 > "$lg_projects_dir"/"$lg_project".githead && cd "$lg_projects_dir"
+diff imgui.githead hello_imgui.githead # this will fail if they differ
+
+
 title "Create virtual env"
 rm -rf venv_ci/
 python3 -m venv venv_ci
 . venv_ci/bin/activate
 which python
 
+
 title "Install requirements"
 pip install -r requirements-dev.txt
 pip install -r requirements.txt
+
 
 title "Run mypy static checker"
 cd "$REPO_DIR"
 mypy .
 
+
 title "Install litgen and its sub-packages (codemanip, srcmlcpp and litgen)"
 cd "$REPO_DIR"
 pip install .
+
 
 title "Build litgensample"
 cd "$REPO_DIR"/example
 pip install .
 
+
 title "Run pytest (will test codemanip, srcmlcpp, litgen, and litgensample)"
 cd "$REPO_DIR"
 pytest
 
+
 title "Build lg-imgui python module"
 cd "$REPO_DIR"/examples_real_libs/imgui
 pip install .
+
 
 title "Build hello-imgui python modules"
 cd "$REPO_DIR"/examples_real_libs/hello_imgui
