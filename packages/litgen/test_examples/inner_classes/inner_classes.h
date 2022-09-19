c9fd2a7dc4424afb3d8950ca06b3b722f2f57dc6
Pascal Thomet <pthomet@gmail.com>
2022-09-19 08:30:21 +0200

    update test_examples

--- packages/litgen/test_examples/inner_classes/inner_classes.h
+++ packages/litgen/test_examples/inner_classes/inner_classes.h
@@ -1,9 +1,18 @@
 struct Parent
 {
-    int a;
+    int a = 1;
     struct Child
     {
-        int b;
+        int b = 2;
+        // int getB() { return b; }
     };
     Child child;
+
+    enum class Item
+    {
+        Zero = 0,
+        One,
+        Two
+    };
+    Item item;
 };
