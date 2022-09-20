78f201fed9fff2f211b0ebd3d66055f267288c21
Pascal Thomet <pthomet@gmail.com>
2022-09-20 15:07:39 +0200

    litgen: publish srcml_code_grouped

--- packages/litgen/__init__.py
+++ packages/litgen/__init__.py
@@ -3,4 +3,5 @@ from litgen._generated_code import GeneratedCode
 
 from litgen._generated_code import CppFileAndOptions, CppFilesAndOptionsList
 from litgen.generate import generate_code, generate_code_for_files, write_generated_code, code_to_pydef, code_to_stub
+from litgen.code_to_adapted_unit import code_to_adapted_unit
 from litgen.internal.cpp_to_python import standard_code_replacements, opencv_replacements, standard_comment_replacements
