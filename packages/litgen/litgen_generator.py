7f2ab2cde3453d48a3ded7dd9acefa0819d73803
Pascal Thomet <pthomet@gmail.com>
2022-09-27 15:10:03 +0200

    litgen: work on boxed_types_test

--- packages/litgen/litgen_generator.py
+++ packages/litgen/litgen_generator.py
@@ -30,8 +30,55 @@ class LitgenGenerator:
         self.lg_context = LitgenContext(options)
         self._generated_codes = []
 
+    def process_cpp_file(self, filename: str) -> None:
+        code = _read_code(self.lg_context.options, filename)
+        self._process_cpp_code(code, filename)
+
+    def write_generated_code(self, output_cpp_pydef_file: str, output_stub_pyi_file: str) -> None:
+        pydef_code = self.pydef_code()
+        stub_code = self.stub_code()
+        code_utils.write_generated_code_between_markers(output_cpp_pydef_file, "pydef", pydef_code)
+        code_utils.write_generated_code_between_markers(output_stub_pyi_file, "stub", stub_code)
+
+        if self.has_boxed_types():
+            boxed_types_cpp_code = self.boxed_types_cpp_code()
+            code_utils.write_generated_code_between_markers(
+                output_cpp_pydef_file, "boxed_types_header", boxed_types_cpp_code
+            )
+
     def options(self) -> LitgenOptions:
         return self.lg_context.options
+
+    def has_boxed_types(self) -> bool:
+        return len(self.lg_context.boxed_types_registry.cpp_boxed_types) > 0
+
+    def pydef_code(self) -> CppCode:
+        pydef_codes = []
+        for generated_code in self._generated_codes_with_boxed_types():
+            decorated_pydef_code = cpp_to_python.surround_cpp_code_with_filename(
+                self.lg_context.options, generated_code.source_filename, generated_code.pydef_code
+            )
+            pydef_codes.append(decorated_pydef_code)
+        pydef_code = "\n\n".join(pydef_codes)
+        return pydef_code
+
+    def stub_code(self) -> PythonCode:
+        stub_codes = []
+        for generated_code in self._generated_codes_with_boxed_types():
+            decorated_stub_code = cpp_to_python.surround_python_code_with_filename(
+                self.lg_context.options, generated_code.source_filename, generated_code.stub_code
+            )
+            stub_codes.append(decorated_stub_code)
+        stub_code = "\n\n".join(stub_codes)
+        return stub_code
+
+    def boxed_types_cpp_code(self) -> CppCode:
+        cpp_codes = []
+        for cpp_boxed_type in self.lg_context.boxed_types_registry.cpp_boxed_types:
+            indent_str = self.lg_context.options.indent_cpp_spaces()
+            cpp_codes.append(boxed_python_type2.boxed_type_cpp_struct_code(cpp_boxed_type, indent_str))
+        r = "".join(cpp_codes)
+        return r
 
     def _process_cpp_code(self, code: str, filename: str) -> None:
         adapted_unit = code_to_adapted_unit(self.lg_context, code)
@@ -40,23 +87,8 @@ class LitgenGenerator:
         generated_code = GeneratedCode(source_filename=filename, stub_code=stub_code, pydef_code=pydef_code)
         self._generated_codes.append(generated_code)
 
-    def process_cpp_file(self, filename: str) -> None:
-        code = _read_code(self.lg_context.options, filename)
-        self._process_cpp_code(code, filename)
-
-    def has_boxed_types(self) -> bool:
-        return len(self.lg_context.boxed_types_registry.cpp_boxed_types) > 0
-
-    def _boxed_types_cpp_code(self) -> CppCode:
-        cpp_codes = []
-        for cpp_boxed_type in self.lg_context.boxed_types_registry.cpp_boxed_types:
-            indent_str = self.lg_context.options.indent_cpp_spaces()
-            cpp_codes.append(boxed_python_type2.boxed_type_cpp_struct_code(cpp_boxed_type, indent_str))
-        r = "".join(cpp_codes)
-        return r
-
     def _boxed_types_generated_code(self) -> Optional[GeneratedCode]:
-        boxed_types_cpp_code = self._boxed_types_cpp_code()
+        boxed_types_cpp_code = self.boxed_types_cpp_code()
 
         standalone_options = LitgenOptions()
         standalone_options.cpp_indent_size = self.options().cpp_indent_size
@@ -76,39 +108,6 @@ class LitgenGenerator:
         else:
             r = [boxed_types_generated_code] + self._generated_codes
             return r
-
-    def _pydef_code(self) -> str:
-        pydef_codes = []
-        for generated_code in self._generated_codes_with_boxed_types():
-            decorated_pydef_code = cpp_to_python.surround_cpp_code_with_filename(
-                self.lg_context.options, generated_code.source_filename, generated_code.pydef_code
-            )
-            pydef_codes.append(decorated_pydef_code)
-        pydef_code = "\n\n".join(pydef_codes)
-        return pydef_code
-
-    def _stub_code(self) -> str:
-        stub_codes = []
-        for generated_code in self._generated_codes_with_boxed_types():
-            decorated_stub_code = cpp_to_python.surround_python_code_with_filename(
-                self.lg_context.options, generated_code.source_filename, generated_code.stub_code
-            )
-            stub_codes.append(decorated_stub_code)
-        stub_code = "\n\n".join(stub_codes)
-        return stub_code
-
-    def write_generated_code(self, output_cpp_pydef_file: str, output_stub_pyi_file: str) -> None:
-
-        pydef_code = self._pydef_code()
-        stub_code = self._stub_code()
-        code_utils.write_generated_code_between_markers(output_cpp_pydef_file, "pydef", pydef_code)
-        code_utils.write_generated_code_between_markers(output_stub_pyi_file, "stub", stub_code)
-
-        if self.has_boxed_types():
-            boxed_types_cpp_code = self._boxed_types_cpp_code()
-            code_utils.write_generated_code_between_markers(
-                output_cpp_pydef_file, "boxed_types_header", boxed_types_cpp_code
-            )
 
 
 def write_generated_code_for_files(
@@ -135,14 +134,14 @@ class LitgenGeneratorTestUtils:
     def code_to_pydef(options: LitgenOptions, code: str) -> str:
         generator = LitgenGenerator(options)
         generator._process_cpp_code(code, "")
-        r = generator._pydef_code()
+        r = generator.pydef_code()
         return r
 
     @staticmethod
     def code_to_stub(options: LitgenOptions, code: str) -> str:
         generator = LitgenGenerator(options)
         generator._process_cpp_code(code, "")
-        r = generator._stub_code()
+        r = generator.stub_code()
         return r
 
 
---
+++ packages/litgen/tests/boxed_types_test.py
@@ -0,0 +1,105 @@
+from codemanip import code_utils
+
+import litgen
+from litgen.internal import boxed_python_type2
+from litgen.litgen_generator import LitgenGenerator
+
+
+def test_boxed_type_cpp_struct_code():
+    options = litgen.LitgenOptions()
+    code = boxed_python_type2.boxed_type_cpp_struct_code("std::string", options.indent_cpp_spaces())
+    code_utils.assert_are_codes_equal(
+        code,
+        """
+        struct BoxedString
+        {
+            std::string value;
+            BoxedString(std::string v = "") : value(v) {}
+            std::string __repr__() const { return std::string("BoxedString(") + value + ")"; }
+        };
+    """,
+    )
+
+
+def test_truc():
+    options = litgen.LitgenOptions()
+    options.fn_params_replace_modifiable_immutable_by_boxed__regex = ".*"
+    cpp_code = """
+    void foo_const(const std::string& s);
+    void foo_non_const(std::string& s);
+    """
+
+    generator = LitgenGenerator(options)
+    generator._process_cpp_code(cpp_code, "file.h")
+
+    # logging.warning(f"\nboxed_types_cpp_code=\n{generator.boxed_types_cpp_code()}")
+    code_utils.assert_are_codes_equal(
+        generator.boxed_types_cpp_code(),
+        """
+        struct BoxedString
+        {
+            std::string value;
+            BoxedString(std::string v = "") : value(v) {}
+            std::string __repr__() const { return std::string("BoxedString(") + value + ")"; }
+        };
+    """,
+    )
+
+    # logging.warning(f"\nstub_code = \n{generator.stub_code()}")
+    code_utils.assert_are_codes_equal(
+        generator.stub_code(),
+        """
+        ####################    <generated_from:BoxedTypes>    ####################
+        class BoxedString:
+            value: str
+            def __init__(self, v: str = "") -> None:
+                pass
+            def __repr__(self) -> str:
+                pass
+        ####################    </generated_from:BoxedTypes>    ####################
+
+
+        ####################    <generated_from:file.h>    ####################
+
+        def foo_const(s: str) -> None:
+            pass
+        def foo_non_const(s: BoxedString) -> None:
+            pass
+        ####################    </generated_from:file.h>    ####################
+    """,
+    )
+
+    code_utils.assert_are_codes_equal(
+        generator.pydef_code(),
+        """
+        ////////////////////    <generated_from:BoxedTypes>    ////////////////////
+        auto pyClassBoxedString = py::class_<BoxedString>
+            (m, "BoxedString", "")
+            .def_readwrite("value", &BoxedString::value, "")
+            .def(py::init<std::string>(),
+                py::arg("v") = "")
+            .def("__repr__",
+                &BoxedString::__repr__)
+            ;
+        ////////////////////    </generated_from:BoxedTypes>    ////////////////////
+
+
+        ////////////////////    <generated_from:file.h>    ////////////////////
+        m.def("foo_const",
+            foo_const, py::arg("s"));
+
+        m.def("foo_non_const",
+            [](BoxedString & s)
+            {
+                auto foo_non_const_adapt_modifiable_immutable = [](BoxedString & s)
+                {
+                    std::string & s_boxed_value = s.value;
+
+                    foo_non_const(s_boxed_value);
+                };
+
+                foo_non_const_adapt_modifiable_immutable(s);
+            },     py::arg("s"));
+        ////////////////////    </generated_from:file.h>    ////////////////////
+    """,
+    )
