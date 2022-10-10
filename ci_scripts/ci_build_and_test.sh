cd2b2f82f654ff0f548a860b84954ffe9aa507c2
Pascal Thomet <pthomet@gmail.com>
2022-10-10 15:12:37 +0200

    work on CI: use poetry

--- ci_scripts/ci_build_and_test.sh
+++ ci_scripts/ci_build_and_test.sh
@@ -4,6 +4,8 @@ set -e  # Exit on error
 set -x  # Trace commands
 
 REPO_DIR=$SCRIPT_DIR/..
+# PIP_INSTALL_EDITABLE="-e"
+PIP_INSTALL_EDITABLE=""
 
 function title {
   set +x
@@ -19,42 +21,18 @@ $decoration
 }
 
 
-title "Check that lg_imgui and lg_imgui_bundle use the same imgui version"
-lg_projects_dir=$REPO_DIR/lg_projects
-lg_project=lg_imgui
-cd "$lg_projects_dir"/$lg_project/external/imgui && git --no-pager log --format="%H" -n 1 > "$lg_projects_dir"/"$lg_project".githead && cd "$lg_projects_dir"
-lg_project=lg_imgui_bundle
-cd "$lg_projects_dir"/$lg_project/external/imgui && git --no-pager log --format="%H" -n 1 > "$lg_projects_dir"/"$lg_project".githead && cd "$lg_projects_dir"
-# this diff fails if they differ and the script will exit
-diff lg_imgui.githead lg_imgui_bundle.githead
-
-
-title "Create virtual env"
-cd $REPO_DIR
-rm -rf venv_ci/
-python3 -m venv venv_ci
-. venv_ci/bin/activate
-which python
-
-
-title "Install requirements"
-cd $REPO_DIR
-pip install -r requirements-dev.txt
+title "Poetry install / add to PATH"
+cd "$REPO_DIR"
+poetry install
+poetry_python_path=$(poetry run which python)
+poetry_python_folder=$(dirname $poetry_python_path)
+export PATH=$poetry_python_folder:$PATH
+echo poetry_python_folder=$poetry_python_folder
 
 
 title "Run mypy static checker"
 cd "$REPO_DIR"
 mypy .
-
-title "Build litgen/integration_tests pip package (lg_mylib)"
-cd "$REPO_DIR"/packages/litgen/integration_tests
-python autogenerate_mylib.py
-pip install .
-
-
-title "Run pytest (will test codemanip srcmlcpp litgen, and litgensample)"
-cd "$REPO_DIR"
-pytest
 
 
 title "autogenerate_all"
@@ -62,19 +40,29 @@ cd "$REPO_DIR"
 ./ci_scripts/autogenerate_all.sh
 
 
+title "Build litgen/integration_tests pip package (lg_mylib)"
+cd "$REPO_DIR"/packages/litgen/integration_tests
+pip install -v $PIP_INSTALL_EDITABLE .
+
+
+title "Run pytest (will test codemanip srcmlcpp litgen, and litgensample)"
+cd "$REPO_DIR"
+pytest
+
+
 title "cmake build all"
 cd "$REPO_DIR"
-mkdir -p ci_build
-cd ci_build
-cmake .. -DLITGEN_CI=ON
+mkdir -p build_ci
+cd build_ci
+poetry run cmake ..
 make -j 4
-
-
-title "pip build lg_imgui_bundle python modules"
-cd "$REPO_DIR"/lg_projects/lg_imgui_bundle
-pip install .
 
 
 title "pip build lg_skbuild_template python modules"
 cd "$REPO_DIR"/lg_projects/lg_skbuild_template
-pip install .
+poetry run pip install -v $PIP_INSTALL_EDITABLE .
+
+
+title "pip build lg_imgui_bundle python modules"
+cd "$REPO_DIR"/lg_projects/lg_imgui_bundle
+pip install -v $PIP_INSTALL_EDITABLE .
--- requirements_update_via_poetry.sh
+++
@@ -1,2 +0,0 @@
-poetry export -f requirements.txt --without-hashes  --output requirements.txt
-poetry export -f requirements.txt --without-hashes  --dev --output requirements-dev.txt
