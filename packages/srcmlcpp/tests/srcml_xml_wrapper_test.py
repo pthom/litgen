e5c451b0beb59237e0f7cbdb8429773700f12b88
Pascal Thomet <pthomet@gmail.com>
2022-10-11 11:03:09 +0200

    srcmlcpp: reorganize tests

--- packages/srcmlcpp/tests/srcml_xml_wrapper_test.py
+++ packages/srcmlcpp/tests/srcml_xml_wrapper_test.py
@@ -3,8 +3,6 @@ from codemanip.code_position import CodePosition
 
 import srcmlcpp.srcmlcpp_main
 from srcmlcpp import SrcmlWrapper
-from srcmlcpp.internal.srcmlcpp_exception_detailed import SrcmlcppExceptionDetailed
-from srcmlcpp.srcmlcpp_exception import SrcmlcppException
 from srcmlcpp.srcmlcpp_options import SrcmlcppOptions
 from srcmlcpp.srcmlcpp_main import code_to_srcml_wrapper
 
@@ -71,60 +69,6 @@ def test_srcml_cpp():
         </decl_stmt>
     """
     code_utils.assert_are_codes_equal(first_decl_xml_str, expected_xml_str)
-
-
-def test_warnings():
-    options = SrcmlcppOptions()
-    options.flag_show_python_callstack = True
-    code = "void foo(int a);"
-
-    cpp_unit = srcmlcpp.code_to_cpp_unit(options, code, filename="main.h")
-    decl = cpp_unit.block_children[0]
-
-    got_exception = False
-    try:
-        raise SrcmlcppExceptionDetailed(decl, "Artificial exception")
-    except SrcmlcppException as e:
-        got_exception = True
-        msg = str(e)
-        for part in [
-            "test_warnings",
-            "function_decl",
-            "main.h:1:1",
-            "void foo",
-            "Artificial exception",
-        ]:
-            assert part in msg
-    assert got_exception
-
-
-def test_warnings_2():
-    options = srcmlcpp.SrcmlcppOptions()
-    code = code_utils.unindent_code(
-        """
-        struct __Foo
-        {
-            int a = 1;
-        };
-    """,
-        flag_strip_empty_lines=True,
-    )
-    cpp_struct = srcmlcpp.srcmlcpp_main.code_first_struct(options, code)
-
-    msg = cpp_struct._warning_message_str("names starting with __ are reserved")
-    code_utils.assert_are_codes_equal(
-        msg,
-        """
-        Warning: names starting with __ are reserved
-        While parsing a "struct", corresponding to this C++ code:
-        Position:1:1
-            struct __Foo
-            ^
-            {
-                int a = 1;
-            };
-    """,
-    )
 
 
 def test_visitor():
---
+++ packages/srcmlcpp/tests/srcmlcpp_exception_test.py
@@ -0,0 +1,60 @@
+from codemanip import code_utils
+
+import srcmlcpp.srcmlcpp_main
+from srcmlcpp.internal.srcmlcpp_exception_detailed import SrcmlcppExceptionDetailed
+from srcmlcpp.srcmlcpp_exception import SrcmlcppException
+from srcmlcpp.srcmlcpp_options import SrcmlcppOptions
+
+
+def test_warnings():
+    options = SrcmlcppOptions()
+    options.flag_show_python_callstack = True
+    code = "void foo(int a);"
+
+    cpp_unit = srcmlcpp.code_to_cpp_unit(options, code, filename="main.h")
+    decl = cpp_unit.block_children[0]
+
+    got_exception = False
+    try:
+        raise SrcmlcppExceptionDetailed(decl, "Artificial exception")
+    except SrcmlcppException as e:
+        got_exception = True
+        msg = str(e)
+        for part in [
+            "test_warnings",
+            "function_decl",
+            "main.h:1:1",
+            "void foo",
+            "Artificial exception",
+        ]:
+            assert part in msg
+    assert got_exception
+
+
+def test_warnings_2():
+    options = srcmlcpp.SrcmlcppOptions()
+    code = code_utils.unindent_code(
+        """
+        struct __Foo
+        {
+            int a = 1;
+        };
+    """,
+        flag_strip_empty_lines=True,
+    )
+    cpp_struct = srcmlcpp.srcmlcpp_main.code_first_struct(options, code)
+
+    msg = cpp_struct._warning_message_str("names starting with __ are reserved")
+    code_utils.assert_are_codes_equal(
+        msg,
+        """
+        Warning: names starting with __ are reserved
+        While parsing a "struct", corresponding to this C++ code:
+        Position:1:1
+            struct __Foo
+            ^
+            {
+                int a = 1;
+            };
+    """,
+    )
