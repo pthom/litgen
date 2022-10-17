342bc48110c2df87cabc03a5da7f34f9fe6510bb
Pascal Thomet <pthomet@gmail.com>
2022-10-17 14:37:36 +0200

    Notes / CMakeLists

--- pybind_native_debug/pybind_native_debug.py
+++ pybind_native_debug/pybind_native_debug.py
@@ -2,28 +2,16 @@
 import os
 import sys
 
-import demodocking  # type:ignore
-
-
-# import play_implot  # type: ignore
-# import play_himgui  # type: ignore
-
-
 THIS_DIR = os.path.dirname(__file__)
 
-path_lg_project = os.path.realpath(f"{THIS_DIR}/../lg_projects/")
 
-path_lg_imgui_playground = path_lg_project + "/lg_imgui/playground"
-path_lg_implot_playground = path_lg_project + "/lg_implot/playground"
-path_lg_imgui_bundle_playground = path_lg_project + "/lg_imgui_bundle/playground"
-sys.path.append(path_lg_imgui_playground)
-sys.path.append(path_lg_implot_playground)
-sys.path.append(path_lg_imgui_bundle_playground)
+def demo_node_editor() -> None:
+    path_lg_imgui_bundle = os.path.realpath(f"{THIS_DIR}/../demos/litgen/lg_imgui_bundle/demos/")
+    print(path_lg_imgui_bundle)
+    sys.path.append(path_lg_imgui_bundle)
+    import demo_node_editor  # type: ignore
 
-# print(path_lg_imgui_playground)
+    demo_node_editor.main()
 
 
-demodocking.main()
-
-# import play_with_imgui  # type: ignore
-# import play_himgui
+demo_node_editor()
