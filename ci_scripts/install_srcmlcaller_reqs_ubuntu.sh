54f7fc3d50b29d179017b9aa4cc7bebd8946d8f4
Pascal Thomet <pthomet@gmail.com>
2022-11-27 16:11:48 +0100

    add install_srcmlcaller_reqs_ubuntu.sh

---
+++ ci_scripts/install_srcmlcaller_reqs_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && sudo apt-get install --no-install-recommends -y \
+    libxml2-dev \
+    libxml2-utils \
+    libxslt1-dev \
+    libarchive-dev \
+    libssl-dev \
+    libcurl4-openssl-dev
