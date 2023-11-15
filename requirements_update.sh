32d92014ce76ec6bd7c522c899cc7bea4b6dafce
Pascal Thomet <pthomet@gmail.com>
2023-11-15 20:57:51 +0100

    update poetry

--- requirements_update.sh
+++ requirements_update.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-poetry export -f requirements.txt --without-hashes --dev > requirements.txt
+poetry export -f requirements.txt --without-hashes --with dev > requirements_dev.txt
+poetry export -f requirements.txt --without-hashes > requirements.txt
