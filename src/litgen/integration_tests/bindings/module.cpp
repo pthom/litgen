c20969a04a4a44345022be47f177102501fbe7c7
Pascal Thomet <pthomet@gmail.com>
2024-01-24 07:51:42 +0100

    CI: simplify build (use tooling from template)

--- src/litgen/integration_tests/bindings/module.cpp
+++ src/litgen/integration_tests/bindings/module.cpp
@@ -11,11 +11,5 @@ void py_init_module_lg_mylib(py::module& m);
 // it will be wrapped in a standard python module `testrunner`
 PYBIND11_MODULE(_lg_mylib, m)
 {
-    #ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
-    #else
-    m.attr("__version__") = "dev";
-    #endif
-
     py_init_module_lg_mylib(m);
 }
