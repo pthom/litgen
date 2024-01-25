a881ed18c7427861e836fe73a3674c511f864334
Pascal Thomet <pthomet@gmail.com>
2024-01-25 11:08:37 +0100

    Add run_all_checks

---
+++ ci_scripts/devel/run_all_checks.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# set -e
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+repo_dir=$(realpath $this_dir/../..)
+
+# Run all checks
+cd $repo_dir
+
+# Catch errors and display error message
+trap 'echo "Error occured on line $LINENO"; exit 1' ERR
+
+echo "Run mypy ."
+mypy .
+echo "Run black ."
+black .
+echo "Run ruff ."
+ruff .
+echo "Run pytest"
+pytest
+echo "Succes!"
