af56b6d6f26e057f92f9f8b6599b0118525cf631
davidlatwe <davidlatwe@gmail.com>
2024-03-27 20:50:35 +0800

    Revert "Workable nanobind generating code"
    This reverts commit e247b5fe4eaf55a25c67594b9638057f8f7f547f.

--- src/litgen/internal/cpp_to_python.py
+++ src/litgen/internal/cpp_to_python.py
@@ -526,7 +526,7 @@ def standard_type_replacements() -> RegexReplacementList:
     \bvoid\s*\* -> Any
     \bvoid\b -> None
 
-    \bpy::ndarray<(.*)> -> np.ndarray
+    \bpy::array\b -> np.ndarray
 
     \bconst\b -> REMOVE
     \bmutable\b -> REMOVE
