ac8322416f23e5623970358fac20d076cc3eca40
Pascal Thomet <pthomet@gmail.com>
2024-10-23 12:04:47 +0200

    p2 added validate_bindings_compilation

--- src/litgen/tests/internal/test_validate_bindings_compilation.py
+++ src/litgen/tests/internal/test_validate_bindings_compilation.py
@@ -3,22 +3,36 @@ from litgen.internal.validate_bindings_compilation import validate_bindings_comp
 
 
 def test_validate_bindings_compilation() -> None:
+    # Validates that the cpp code can be compiled into bindings and
+    # that the generated Python bindings work as expected.
+    # **This kind of test is slow**, do not use it extensively in CI.
+
     code = """
-    struct Foo {
-        int x = 1;
-    };
+struct Foo
+{
+    int x = 1;
+};
     """
-    bind_type = litgen.BindLibraryType.pybind11
+    python_test_code = """
+import validate_bindings_compilation
 
-    options = litgen.LitgenOptions()
-    options.bind_library = bind_type
-    generated_code = litgen.generate_code(options, code)
-    success = validate_bindings_compilation(
-        cpp_bound_code=code,
-        bind_library_type=bind_type,
-        generated_code=generated_code,
-        build_dir="/Users/pascal/dvp/OpenSource/ImGuiWork/_Bundle/litgen/src/litgen/tests/internal/ppp",
-        remove_build_dir_on_success=False
-        )
+def test_validate_bindings_compilation() -> None:
+    foo = validate_bindings_compilation.Foo()
+    assert hasattr(foo, 'x')
+    assert foo.x == 1
+    """
 
-    assert success
+    for bind_type in litgen.BindLibraryType:
+        options = litgen.LitgenOptions()
+        options.bind_library = bind_type
+
+        success = validate_bindings_compilation(
+            cpp_code=code,
+            options=options,
+            remove_build_dir_on_success=False,
+            python_test_code=python_test_code,
+            show_logs=True,
+            python_module_name="validate_bindings_compilation",
+            work_dir="/Users/pascal/dvp/OpenSource/ImGuiWork/_Bundle/litgen/src/litgen/tests/internal/ppp"
+            )
+        assert success
