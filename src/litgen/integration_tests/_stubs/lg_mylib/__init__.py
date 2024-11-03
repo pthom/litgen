ef0df84a78c6922aa1d64ba8dec5e70d1198568f
davidlatwe <davidlatwe@gmail.com>
2024-11-04 07:55:21 +0800

    Don't import binding before configured

--- src/litgen/integration_tests/_stubs/lg_mylib/__init__.py
+++ src/litgen/integration_tests/_stubs/lg_mylib/__init__.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 
 def _get_binding_type():
     import os
+    import sys
+
+    if "-m" in sys.argv:
+        # Possibly running `python -m lg_mylib.use`.
+        return
 
     binding_type = ""
     dirname = os.path.dirname(__file__)
@@ -18,5 +23,8 @@ def _get_binding_type():
 _binding_type = _get_binding_type()
 if _binding_type == "nanobind":
     from lg_mylib._lg_mylib_nanobind import *  # type: ignore # noqa
-if _binding_type == "pybind":
+elif _binding_type == "pybind":
     from lg_mylib._lg_mylib_pybind import *  # type: ignore # noqa
+else:
+    import warnings
+    warnings.warn("Binding not imported.")
