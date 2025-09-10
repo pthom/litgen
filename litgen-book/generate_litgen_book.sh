7aae82d4880bee7063d3fc46aa7d049ff7c4c3f5
Pascal Thomet <pthomet@gmail.com>
2025-09-10 17:54:26 +0200

    litgen-book: add doc / custom bindings + add full pdf

--- litgen-book/generate_litgen_book.sh
+++ litgen-book/generate_litgen_book.sh
@@ -9,3 +9,7 @@ rm -rf _build
 rm -rf $REPO_DIR/docs/litgen_book
 jupyter-book build .
 cp -a _build/html $REPO_DIR/docs/litgen_book
+
+# Build PDF version (uncomment if needed)
+jupyter-book build . --builder pdfhtml
+cp _build/pdf/book.pdf $REPO_DIR/docs/litgen_book/litgen_book.pdf
